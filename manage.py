95146ff126137967a77f97d15c7f6dc0525cf957
Alfredo Yebra <root1@rrcs-67-78-51-76.sw.biz.rr.com>
2017-10-21 06:30:09 -0500

    Add random image generation.

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
+import urllib2
+
+from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
 from flask_assets import Environment, Bundle
+from google import google
 
 # Configure app.
 app = Flask(__name__)
@@ -10,10 +14,21 @@ assets = Environment(app)
 scss = Bundle('scss/base.scss', filters='pyscss', output='css/all.css')
 assets.register('scss_all', scss)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
-
-
+    if len(img_urls) < 15:
+        for topic in ['baseball', 'food']:
+            if len(img_urls) > 15:
+                continue
+            for result in google.search(topic, 1):
+                content = urllib2.urlopen(result.link).read()
+                soup = BeautifulSoup(content, 'html.parser')
+                for link in soup.find_all('img'):
+                    if link.get('src'):
+                        img_urls.append(link.get('src'))
+    if request.method == 'GET':
+        return render_template('index.html', urls=img_urls or [])
+    return render_template('404.html')
 if __name__ == '__main__':
+    img_urls = []
     app.run()
